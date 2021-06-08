fc32fd05cee44bcd52784f41dd3d9bee04bf5b5c
Kylee Bijerring <kylee24b@gmail.com>
2021-06-07 18:59:19 -0600

    new

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,30 @@
+ let animals = {
+    apikey = "9SyoHw6D",
+resucePet: function () {
+  fetch(
+    "https://api.rescuegroups.org/http/v2.json"
+    +this.apikey
+  )
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+}
+  }
+
+
+petApiKey= "FBrjvWBdiNTgthvFmtSElrrE5KLkDN9um8KrVQKxKkDCIJJsjV"
+petSecret = "I6rmtBHl0TCA9Mj4SAWKAXANtRUBfXNIcRFCGeJS"
+petToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJGQnJqdldCZGlOVGd0aHZGbXRTRWxyckU1S0xrRE45dW04S3JWUUt4S2tEQ0lKSnNqViIsImp0aSI6IjNmMzcwNTliYWZlYjYwNmUzY2Y4NmM2NGU2Mzc3NTdmMGNjYTMzN2I3ZmMyNGQzMmZiNWE4YzNlNWRiZDNkYTczNDM1ZjQyYTJiNGQzNzhlIiwiaWF0IjoxNjIyOTMwNzY5LCJuYmYiOjE2MjI5MzA3NjksImV4cCI6MTYyMjkzNDM2OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.QrB4deaOXvBxjaJmaGHMWn0xy9T-bxOb4jim0PiXdZHjkgHMzJaq1DmetbCBdR_z5MSIkCu2vRObDxfXZoAuHuRDqkICPlpHRYXWKZ1vXlBbn7tgHDUyF0rWojF9NSsYYafsboisJhu3Qm0E2ASvJV2uCMqju4EOFLrvKSONFVYh0ef3PPWvehNGTzMX0igTO_end9o7V7F_c_HYCfrHnDmrdryg2PDMLpmPFyCsV60sqS4tQSPwIv2K5gaxfHFzOznQnKZvqtoNM8pBUX1iGittq3R8WDPXuKyXp0OFTlMtHU9yVjIYYgKwTxA7bzEKHiM9Pf7HrFlv-e63pKzbhw"
+url =  "https://api.petfinder.com/v2"
+
+
+
+
+
+
+
+
 //for the menu//
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-link");
@@ -11,7 +38,3 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
-
-
-
-apiKey= "FBrjvWBdiNTgthvFmtSElrrE5KLkDN9um8KrVQKxKkDCIJJsjV"
