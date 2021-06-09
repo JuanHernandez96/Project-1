87d5fc4065898db289508379a20fb8a4770f7015
Juan Hernandez <juanh22@gmail.com>
2021-06-09 13:53:53 -0600

    added basic function to localstorage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,15 +35,16 @@ function getAnimal(animalType, location) {
 
         .done(function (data) {
           console.log(data)
-          var displayResults = document.querySelector(".displayResults")
+          var displayResults = document.querySelector("#displayResults")
           var animalsArray = data.animals
                 for (let index = 0; index < animalsArray.length; index++) {
                      if(animalsArray[index].photos[0]){
+                       console.log(animalsArray[index].id)
                       displayResults.innerHTML+=`
                       <h2>${animalsArray[index].name} </h2>
                       <h4>${animalsArray[index].contact.email}</h4>
                       <img src="${ animalsArray[index].photos[0].medium}">             
-                      <a><i onclick="save_data()" id="heart" class="fas fa-heart"></i></a>
+                      <button data-id="${animalsArray[index].id}"><i onclick="save_data()" id="heart" class="fas fa-heart"></i></button>
                       
                       `
                      }
@@ -53,16 +54,37 @@ function getAnimal(animalType, location) {
 
     });
 }
+function save_data(){
+  var newFavorite = $(this).attr("data-id")
+  // this needs to grab data-id value from line 47 animalsArray[index].id
 
-    $.ajax({
-      method: "GET",
-      url:
-        "https://api.petfinder.com/v2/animals?type=" +
-        animalType +
-        "&location=" +
-        location,
-      headers: { Authorization: "Bearer " + msg.access_token },
-    }).done(function (data) {
-      console.log(data);
-      var displayResults = document.querySelector("#displayResults")
-    })
+  saveAnimal.push(newFavorite)
+
+  window.localStorage.setItem("animalArray", JSON.stringify(saveAnimal))
+}
+var saveAnimal = JSON.parse(window.localStorage.getItem("animalArray")) || []
+
+
+
+function displayFavorite() {
+  // either get scores from localstorage or set to empty array
+  var savedAnimal = JSON.parse(window.localStorage.getItem("animalArray")) || [];
+  savedAnimal.forEach(function() {
+
+
+  });
+}
+
+    // $.ajax({
+    //   method: "GET",
+    //   url:
+    //     "https://api.petfinder.com/v2/animals?type=" +
+    //     animalType +
+    //     "&location=" +
+    //     location,
+    //   headers: { Authorization: "Bearer " + msg.access_token },
+    // }).done(function (data) {
+    //   console.log(data);
+    //   var displayResults = document.querySelector("#displayResults")
+    //   displayResults.append()
+    // })
