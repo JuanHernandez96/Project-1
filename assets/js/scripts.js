27ddec8488688818c1799f7ca79aa5360d663a78
Carolina Reedy <carinvid@gmail.com>
2021-06-09 14:56:25 -0600

    fixing errors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,38 +21,13 @@ function getAnimal(animalType, location) {
   $.ajax({
     method: "POST",
     url: "https://api.petfinder.com/v2/oauth2/token",
-    data: { grant_type: "client_credentials", client_id: "91r5U7c01YadVDHmYCPyaE8vMuFKg35qriDBIPa5s0NOFIxnaz", client_secret: "WbfUIeUyx15e8kSqqr74SUq6kDyayTFHtq3kBOgx" }
-  })
-    .done(function (msg) {
-      console.log(msg)
-
-
-      $.ajax({
-        method: "GET",
-        url: "https://api.petfinder.com/v2/animals?type=" + animalType   + "&location=" + location,
-        headers: { Authorization: "Bearer " + msg.access_token }
-      })
-
-        .done(function (data) {
-          console.log(data)
-          var displayResults = document.querySelector(".displayResults")
-          var animalsArray = data.animals
-                for (let index = 0; index < animalsArray.length; index++) {
-                     if(animalsArray[index].photos[0]){
-                      displayResults.innerHTML+=`
-                      <h2>${animalsArray[index].name} </h2>
-                      <h4>${animalsArray[index].contact.email}</h4>
-                      <img src="${ animalsArray[index].photos[0].medium}">             
-                      <a><i onclick="save_data()" id="heart" class="fas fa-heart"></i></a>
-                      
-                      `
-                     }
-                    
-                }
-        });
-
-    });
-}
+    data: {
+      grant_type: "client_credentials",
+      client_id: "91r5U7c01YadVDHmYCPyaE8vMuFKg35qriDBIPa5s0NOFIxnaz",
+      client_secret: "WbfUIeUyx15e8kSqqr74SUq6kDyayTFHtq3kBOgx",
+    },
+  }).done(function (msg) {
+    console.log(msg);
 
     $.ajax({
       method: "GET",
@@ -62,26 +37,27 @@ function getAnimal(animalType, location) {
         "&location=" +
         location,
       headers: { Authorization: "Bearer " + msg.access_token },
-    }).done(function (data) {
+    })
+    .done(function (data) {
       console.log(data);
       var displayResults = document.querySelector("#displayResults");
-
       var animalsArray = data.animals;
-
       for (let index = 0; index < animalsArray.length; index++) {
         if (animalsArray[index].photos[0]) {
+          console.log(animalsArray[index].id);
           displayResults.innerHTML += `
-                       <h2>Name: ${animalsArray[index].name} </h2>
-                       <h2>Gender: ${animalsArray[index].gender}</h2>
-                       <h4>Contact: ${animalsArray[index].contact.email}</h4>
-                      <img src="${animalsArray[index].photos[0].medium}"> 
-                      <a href=${animalsArray[index].photos[0].medium}><i class="fas fa-heart"></i></a>         
+                      <h2>${animalsArray[index].name} </h2>
+                      <h4>${animalsArray[index].contact.email}</h4>
+                      <img src="${animalsArray[index].photos[0].medium}">             
+                      <button data-id="${animalsArray[index].id}"><i onclick="save_data()" id="heart" class="fas fa-heart"></i></button>
+                      
                       `;
         }
       }
     });
   });
 }
+
 // /*button for save fav*/
 //
 //   );
