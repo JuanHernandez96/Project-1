ec731fa066092d8b1ff5641018b0a1ac485dadae
Juan Hernandez <juanh22@gmail.com>
2021-06-13 13:18:59 -0600

    removed https

--- assets/js/Favorite.js
+++ assets/js/Favorite.js
@@ -16,8 +16,7 @@ function displayFavorite() {
       $.ajax({
         method: "GET",
         url: "https://api.petfinder.com/v2/animals/" + id,
-        headers: { Authorization: "Bearer" + msg.access_token,
-      "Access-Control-Allow-Origin": "https://juanhernandez96.github.io/Project-1/favorite.html"},
+        headers: { Authorization: "Bearer" + msg.access_token},
       }).done(function (data)  {
         console.log(data);
         var displayFavorite = document.querySelector("#displayFavorite");
